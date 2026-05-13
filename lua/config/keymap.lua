a423674f555a7842c6836ae55d3096c0c708c1dc
soroush <soroush.safari1992@gmail.com>
2026-05-13 19:55:11 +0330

    build

--- lua/config/keymap.lua
+++ lua/config/keymap.lua
@@ -24,3 +24,6 @@ vim.keymap.set({'n', 'x'}, 'gp', '"+p')
 -- move lines
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv'")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv'")
+vim.keymap.set('n', '<leader>b', '<cmd>Bufferin<cr>', { desc = 'Toggle Bufferin' })
+
+
